1ce67b042d1f89eba146025a3c6a6865e802f9cd
Jeff Sparkes <jsparkes@gmail.com>
2016-03-19 15:20:25 -0400

    Redo utility module using bit logic functions to reduce changes required. 100+ errors, 300 warnings

--- utility.fs
+++ utility.fs
@@ -3,24 +3,37 @@ module Utility
 open Type
 (* Just some useful stuff that has no good home *)
 
-// Use Map<int, int> in C#
+// Use Map<int, int> in F#
 //module IntMap = Map.Make(struct type t = int let compare = compare end)
 
-let byte_of_int x =
-  x &&& 0xff
+// Make functions with OCaml names
+let (mod) a b = a % b
+let (land) a b = a &&& b
+let lnot n = ~~~ n
+let (lor) a b = a ||| b
+let (lsl) a b = a <<< b
+let (lsr) a b = a >>> b
+// Is this correct?
+let (asr) a b = a >>> b
 
-let string_of_char (x : char) =
+// We're not using the byte type to be more consistent
+// with the OCaml code.
+let char_of_int b = char b
+let int_of_char c = int c
+
+type List<'a> with
+  static member fold_left a b c = List.fold a b c
+
+// ==========
+
+let byte_of_int x =
+  x land 0xff
+  
+let string_of_char (x : char)=
   string(x)
 
-// Not using byte type for greater compatibility with OCaml
-let string_of_byte (b : int) =
-  string_of_char (char b)
-
-let char_of_int n =
-  char n
-
-let int_of_char (c : char) =
-  int c
+let string_of_byte b =
+  string_of_char (char_of_int b)
 
 let truncate text length =
   if (String.length text) > length then text.Substring(0, length)
@@ -44,7 +57,7 @@ let right_string (text : string) index =
   text.Substring(index, ((String.length text) - index))
 
 let break_string (text : string) (target : char) =
-  let index = text.IndexOf(target)
+  let index = text.IndexOf(target) in
   let left = left_string text index in
   let right = right_string text (index + 1) in
   (left, right)
@@ -58,7 +71,7 @@ let replace_at original_text index new_text =
 let accumulate_strings to_string items =
   let folder text item =
     text + (to_string item) in
-  List.fold folder "" items
+  List.fold_left folder "" items
 
 let accumulate_strings_loop to_string start max =
   let rec aux acc i =
@@ -67,7 +80,7 @@ let accumulate_strings_loop to_string start max =
   aux "" start
 
 let unsigned_word word =
-  ((word % 65536) + 65536) % 65536
+  ((word mod 65536) + 65536) mod 65536
 
 let signed_word word =
   let canonical = unsigned_word word in
@@ -75,6 +88,10 @@ let signed_word word =
 
   (* Helper method that takes an item and a function that produces related items.
      The result is the transitive closure of the relation. *)
+
+  (* TODO: This is not very efficient because of the call to List.mem in there.
+     TODO: A solution involving an immutable set would be more performant for
+     TODO: large closures. *)
 
 let transitive_closure_many items relation =
   let rec merge related set stack =
@@ -96,7 +113,7 @@ let transitive_closure item relation =
 
 let reflexive_closure_many items relation =
   let t = transitive_closure_many items relation in
-  List.fold (fun s i -> if Set.contains i s then s else Set.add i s) t items
+  List.fold_left (fun s i -> if Set.contains i s then s else Set.add i s) t items
 
 let reflexive_closure item relation =
   reflexive_closure_many [item] relation
@@ -132,27 +149,27 @@ let size6 = Bit_size 6
 let size7 = Bit_size 7
 
 let fetch_bit (Bit_number n) word =
-  (word &&& (1 <<< n)) >>> n = 1
+  (word land (1 lsl n)) lsr n = 1
 
 let clear_bit (Bit_number n) word =
-  word &&& (~~~ (1 >>> n))
+  word land (lnot (1 lsl n))
 
 let set_bit (Bit_number n) word =
-  word ||| (1 <<< n)
+  word lor (1 lsl n)
 
 let set_bit_to n word value =
   if value then set_bit n word
   else clear_bit n word
 
 let fetch_bits (Bit_number high) (Bit_size length) word =
-  let mask = ~~~ (-1 <<< length) in
-  (word >>> (high - length + 1)) &&& mask
+  let mask = lnot (-1 lsl length) in
+  (word lsr (high - length + 1)) land mask
 
 let display_bytes get_byte first length =
   let blocksize = 16 in
   let to_string i =
     let header =
-      if i % blocksize = 0 then Printf.sprintf "\n%06x: " i
+      if i mod blocksize = 0 then Printf.sprintf "\n%06x: " i
       else "" in
     let byte = get_byte (Byte_address i) in
     let contents = Printf.sprintf "%02x " byte in
@@ -167,6 +184,7 @@ let write_file filename (text : string) =
   System.IO.File.WriteAllBytes(filename, System.Text.ASCIIEncoding.ASCII.GetBytes(text))
 
 let rec first_match items predicate =
+  // List.tryFind items predicate
   match items with
   | h :: t -> if predicate h then Some h else first_match t predicate
   | [] -> None
@@ -234,7 +252,7 @@ let dereference_string address bytes =
     failwith "address out of range"
   else
     let (Byte_address addr) = address in
-    int bytes.[addr]
+    int_of_char bytes.[addr]
 
 let address_of_high_byte (Word_address address) =
   Byte_address address
@@ -244,17 +262,3 @@ let address_of_low_byte (Word_address address) =
 
 let byte_addr_to_word_addr (Byte_address address) =
   Word_address address
-
-type List<'a> with
-  static member fold_left a b c = List.fold a b c
-
-let (mod) a b = a % b
-
-let (land) a b = a &&& b
-let (lor) a b = a ||| b
-
-let (lsl) a b = a <<< b
-let (lsr) a b = a >>> b
-
-// Is this correct?
-let (asr) a b = a >>> b
