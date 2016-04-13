7be9be71990f80afce9676196d65d0e377240dfb
Jeff Sparkes <jsparkes@gmail.com>
2016-04-13 11:24:32 -0400

    Allow a file name as the first argument.

--- flathead.fs
+++ flathead.fs
@@ -1,9 +1,21 @@
 module Flathead
 
 open Type
+open System.IO
 
-let story = Story.load "../../MiniZork.Z3" ;;
-let screen = Screen.make (Character_height 50) (Character_width 80) ;;
-let interpreter = Interpreter.make story screen ;;
-let debugger = Debugger.make interpreter ;;
-Debugger.run debugger ;;
+let defaultFiles = [ "MiniZork.Z3"; "../../MiniZork.Z3" ]
+
+[<EntryPoint>]
+let main args = 
+    let name =
+        if args.Length > 0 then
+            args.[0]
+        else
+            List.filter File.Exists defaultFiles |> List.head
+
+    let story = Story.load name
+    let screen = Screen.make (Character_height 50) (Character_width 80)
+    let interpreter = Interpreter.make story screen
+    let debugger = Debugger.make interpreter
+    Debugger.run debugger
+    0
